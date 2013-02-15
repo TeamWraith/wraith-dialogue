60a551da794de2b0c42905c890e4d83c44164e7d
Halvor <stektpotet@gmail.com>
2013-02-15 22:57:37 +0100

    Update src/net/teamwraith/npctalk/infoReadify.java

--- src/net/teamwraith/npctalk/infoReadify.java
+++ src/net/teamwraith/npctalk/infoReadify.java
@@ -8,11 +8,15 @@ public class infoReadify {
 		System.out.println(info.getActorName() + " is my name.");
 		System.out.println("I'm a " + info.getSex().toLowerCase());
 		System.out.println("I'm also a " + info.getType().toLowerCase());
+		System.out.println("I'm of the race " + info.getRace().toLowerCase());
+		System.out.println("I'm " + info.getAge() + " years old :3");
 	}
 
 	public static void set(InfoReadable info){
 		info.setActorName("Ghat");
 		info.setSex(2);
 		info.setType(1);
+		info.setRace("Owl");
+		info.setAge(32);
 	}
 }
